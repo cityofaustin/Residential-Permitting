16ffceea12685292dce92222ce4058f574721e51
Sarah Rudder <sarah.rudder@austintexas.gov>
2017-04-19 13:45:37 -0500

    update USWDS to 1.0

--- assets/_js/uswds/start.js
+++ assets/_js/uswds/start.js
@@ -5,9 +5,11 @@
  * missing from older browsers, so must be loaded first.
  */
 require('./initializers/polyfills');
-require('./initializers/header');
+
 require('./initializers/accordions');
+require('./initializers/banner');
 require('./initializers/footer');
-require('./initializers/skip-nav');
 require('./initializers/forms');
-require('./initializers/politespace');
+require('./initializers/navigation');
+require('./initializers/search');
+require('./initializers/skip-nav');
