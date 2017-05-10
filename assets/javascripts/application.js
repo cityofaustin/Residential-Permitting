66c748ec1220c1fd315a3cac26ec079385a09699
Sarah Rudder <sarah.rudder@austintexas.gov>
2017-05-10 16:07:43 -0500

    Let's just use JQuery instead of the USWDS modules

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -3,8 +3,8 @@
 ---
 
 // Vender JS
- -{% include_relative vendor/jquery.min.js %}
- -{% include_relative vendor/uswds.min.js %}
+{% include_relative vendor/jquery.min.js %}
+{% include_relative vendor/uswds.min.js %}
 
  // Add your JS sripts here
- -{% include_relative foo.js %}
+{% include_relative scripts.js %}
