a6b7d3c67ed8f7b7b8d32eb61960c923a1dfeeba
Sarah Rudder <sarah.rudder@austintexas.gov>
2017-05-10 13:41:40 -0500

    Set up glossary asides

--- src-js/start.js
+++ src-js/start.js
@@ -1,1 +1,15 @@
-console.log("Hi");
+'use strict';
+
+var select = require('../node_modules/uswds/src/js/utils/select');
+
+function initGlossaryAsides() {
+  var asides = select('.glossary-aside');
+  asides.forEach(function (el) {
+    var attachTo = el.previousElementSibling;
+    var newHTML = "<span class='breakout'>" + attachTo.innerHTML + "</span>" + el.outerHTML;
+    attachTo.className += " insert-glossary-here";
+    attachTo.innerHTML = newHTML;
+    el.remove();
+  });
+}
+initGlossaryAsides();
